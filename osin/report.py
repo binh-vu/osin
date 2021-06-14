f10dcc9eb59d4686654a2002326a7cffdd5f57c3
Binh Vu <binh@toan2.com>
2021-06-13 22:36:36 -0700

    add report

--- osin/report.py
+++ osin/report.py
@@ -1,3 +1,5 @@
+import copy
+import itertools
 from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, List
@@ -12,20 +14,35 @@ class ExecutorType(Enum):
 @dataclass
 class ReportDataQuery:
     executor_type: ExecutorType
-    executor_params: Dict[str, str]
-    # arguments: Dict[str, Optional[str]]
+    arguments: List[str]
 
 
 @dataclass
 class Report:
+    name: str
+    description: str
     # parameters of the report and their possible values
     parameters: Dict[str, List[str]]
     # query to obtain data of the report
     data_query: ReportDataQuery
 
-    def query(self, parameters: Dict[str, List[str]]):
-        # TODO: retrieve executor based on type
+    def query(self, parameters: Dict[str, List[str]], executor_id: str):
+        # TODO: retrieve executor from an executor pool
         assert self.data_query.executor_type == ExecutorType.Bash
         executor = BashExecutor(**self.data_query.executor_params)
+        args = self.data_query.arguments
+        runs = []
+        arg_replacement = {}
+        for i, arg in args:
+            if arg.startswith("%%") and arg.endswith("%%") and arg[2:-2] in parameters:
+                arg_replacement[arg[2:-2]] = i
+        for values in itertools.product(*parameters.values()):
+            run_args = copy.copy(args)
+            for k, v in zip(parameters.keys(), values):
+                run_args[arg_replacement[k]] = v
+            runs.append(run_args)
 
+        # submit them to the executor to queue them.
+        for run_args in runs:
+            executor.exec(run_args)
 
