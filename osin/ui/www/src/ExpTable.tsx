86185b141c8e86d322464a4a170f6762ba54aefa
Binh Vu <binh@toan2.com>
2021-07-02 10:41:03 -0700

    wip

--- osin/ui/www/src/ExpTable.tsx
+++ osin/ui/www/src/ExpTable.tsx
@@ -1,13 +1,101 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
-  useParams
+  useParams,
+  withRouter,
+  RouteComponentProps
 } from "react-router-dom";
 
 import { Table, Button, TablePaginationConfig, Checkbox } from 'antd';
 import axios from 'axios';
 
-interface Props {
+interface Props extends RouteComponentProps<{ tableName: string }> {
+}
 
+interface State {
+  records: { [id: string]: string }[],
+  columns: any[],
+  loading: boolean,
+  showDeleted: boolean,
+  descending: boolean,
+}
+
+class ExpTable2 extends React.Component<Props, State> {
+  public state: State = {
+    records: [],
+    columns: [],
+    loading: true,
+    showDeleted: false,
+    descending: true,
+  }
+
+  get tableName() {
+    return this.props.match.params.tableName;
+  }
+
+  componentDidMount = () => {
+    this.reloadData();
+  }
+
+  componentDidUpdate = (props: Props) => {
+    if (this.tableName !== props.match.params.tableName) {
+      this.reloadData();
+    }
+  }
+
+  public reloadData = () => {
+    this.setState({ loading: true });
+    query(this.tableName, 500, 0, this.state.showDeleted, this.state.descending)
+      .then(({ records, columns }) => {
+        columns.push({
+          title: 'ACTIONS',
+          key: '__action_58172__',
+          render: (text: string, record: any) => {
+            return <React.Fragment>
+              <Button type="primary" danger={true} onClick={() => this.deleteRecord(record.id)}>Delete</Button>
+              {this.state.showDeleted && record.deleted ? <Button type="primary" className="ml-4">Restore</Button> : null}
+            </React.Fragment>
+          }
+        });
+        this.setState({ loading: false, records, columns });
+      });
+  }
+
+  public deleteRecord = (recordId: string) => {
+    this.setState({ loading: true });
+    axios.delete(`/api/v1/runs/${recordId}`)
+      .then(() => {
+        this.reloadData();
+      });
+  }
+
+  public render() {
+    return <React.Fragment>
+      <div className="mt-8 mb-8">
+        <Checkbox
+          onChange={(e) => this.setState({ showDeleted: e.target.checked })}
+          checked={this.state.showDeleted}>Show deleted runs</Checkbox>
+        <Checkbox
+          onChange={(e) => this.setState({ descending: e.target.checked })}
+          checked={this.state.descending}>Sort Descending</Checkbox>
+      </div>
+      <Table
+        size="small"
+        dataSource={this.state.records}
+        rowKey={"id"}
+        pagination={{
+          total: 1000,
+          current: 1,
+          pageSize: 50,
+          pageSizeOptions: ["5", "10", "20", "50", "100", '200', '500', '1000'],
+          showSizeChanger: true,
+          showTotal: (total: number) => `Total ${total} items`,
+        }}
+        columns={this.state.columns}
+        loading={this.state.loading}
+        onChange={(pagination) => this.setState({})}
+      />
+    </React.Fragment>;
+  }
 }
 
 /**
@@ -36,7 +124,7 @@ async function query(tableName: string, limit: number, offset: number, includeDe
     for (let cname of Object.keys(record)) {
       if (columns[cname] === undefined) {
         columns[cname] = {
-          title: cname,
+          title: cname.toUpperCase(),
           dataIndex: cname,
           key: cname
         };
@@ -44,30 +132,73 @@ async function query(tableName: string, limit: number, offset: number, includeDe
     }
   }
   columns = Object.values(columns);
-  columns.push({
-    title: 'Actions',
-    key: '__action_58172__',
-    render: (textt: string, record: any) => {
-      return <Button type="primary" danger={true}>Delete</Button>
-    }
-  });
-
   return { records, columns };
 }
 
-export default function ExpTable(props: Props) {
-  let { tableName } = useParams<{ tableName: string }>();
-  let [records, setRecords] = useState('records');
-  let [tableInfo, setTableInfo] = useState({
-    columns: [],
-    loading: true,
-  });
+export default withRouter(ExpTable2);
 
-  // query table from the server
-  useEffect(() => {
-    // setTableInfo({ ...tableInfo, loading: true });
-    query(tableName, 500, 0, true, true);
-  });
+// function ExpTable() {
+//   let { tableName } = useParams<{ tableName: string }>();
+//   let [state, setState] = useState({
+//     version: 0,
+//     records: [],
+//     columns: [],
+//     loading: true,
+//     showDeleted: false,
+//     descending: true,
+//   });
 
-  return <h2>{tableName} haha</h2>;
-}
+//   function deleteRecord(recordId: string) {
+//     setState({ ...state, loading: true });
+//     axios.delete(`/api/v1/runs/${recordId}`)
+//       .then(() => {
+//         setState({ ...state, version: state.version + 1 });
+//       });
+//   }
+
+//   // query table from the server
+//   useEffect(() => {
+//     setState({ ...state, loading: true });
+//     query(tableName, 500, 0, state.showDeleted, state.descending)
+//       .then(({ records, columns }) => {
+//         columns.push({
+//           title: 'ACTIONS',
+//           key: '__action_58172__',
+//           render: (text: string, record: any) => {
+//             return <React.Fragment>
+//               <Button type="primary" danger={true} onClick={() => deleteRecord(record.id)}>Delete</Button>
+//               {state.showDeleted && record.deleted ? <Button type="primary" className="ml-4">Restore</Button> : null}
+//             </React.Fragment>
+//           }
+//         });
+//         setState({ ...state, loading: false, records, columns });
+//       });
+//   }, [tableName, state.showDeleted, state.descending, state.version]);
+
+//   return <React.Fragment>
+//     <div className="mt-8 mb-8">
+//       <Checkbox
+//         onChange={(e) => setState({ ...state, showDeleted: e.target.checked })}
+//         checked={state.showDeleted}>Show deleted runs</Checkbox>
+//       <Checkbox
+//         onChange={(e) => setState({ ...state, descending: e.target.checked })}
+//         checked={state.descending}>Sort Descending</Checkbox>
+//     </div>
+//     <Table
+//       size="small"
+//       dataSource={state.records}
+//       rowKey={"id"}
+//       pagination={{
+//         total: 1000,
+//         current: 1,
+//         pageSize: 50,
+//         pageSizeOptions: ["5", "10", "20", "50", "100", '200', '500', '1000'],
+//         showSizeChanger: true,
+//         showTotal: (total: number) => `Total ${total} items`,
+//       }}
+//       columns={state.columns}
+//       loading={state.loading}
+//       onChange={(pagination) => setState({ ...state })}
+//     />
+//   </React.Fragment>;
+// }
